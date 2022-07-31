044915965d0ccc4b5a5b5ed530e570f7d6a7b36b
ely <eouldsmail@gmail.com>
2022-07-31 18:22:02 +0000

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,42 @@
+/** @jsx h */
+import { h } from "preact";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import {
+  deleteCookie,
+  setCookie,
+} from "https://deno.land/std@0.149.0/http/cookie.ts";
+
+import { hashPassword } from "../utiles/hash_passord.ts";
+//
+import { getUserByName } from "../db_services/users/getone.ts";
+import Nav from "../islands/Nav.tsx";
+import { IToken } from "../db_services/tokens/interface.ts";
+import { createToken } from "../db_services/tokens/add.ts";
+
+interface IMessage {
+  message: string;
+  isLogin: boolean;
+}
+
+export default function Home() {
+  return (
+    <div>
+      <Nav isLogin={false} />
+      <div>
+        <h1>home page</h1>
+      </div>
+      <div>
+        this is demo/example for how authentificate fresh web site users, I use
+        postgresql as database and I use cookies stategie for authentificate to
+        read and set user state(in http request and response objects) instead of
+        using jwt, the user state is managed server side first.<br />
+
+        for more details you visite ...link to github repo ... <br />
+
+        for getting quick idea about this demo just test this site ...link to
+        the site...( register, access protected ressources,login, home ) <br />
+        I made it without any style to let you focus on login and not the ui
+      </div>
+    </div>
+  );
+}
